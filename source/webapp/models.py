b2d5200755737fa02b034e427b7565e532e26983
violettalivada <vilkalivada@gmail.com>
2020-10-22 22:21:13 +0600

    Добавлена модель картинки

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+    image = models.ImageField(null=False, blank=False, upload_to='pics', verbose_name='Фотография')
+    description = models.TextField(null=False, blank=False, max_length=2000, verbose_name='Подпись')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    author = models.CharField(null=False, blank=False, max_length=50, verbose_name='Автор')
+
+    def __str__(self):
+        return self.description
+
+    class Meta:
+        verbose_name = 'Фотография'
+        verbose_name_plural = 'Фотографии'
